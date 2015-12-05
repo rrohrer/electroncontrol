cc6fb647d932ebbb57e585d043d7e19983a322d6
Ryan Rohrer <ryan.rohrer@gmail.com>
2015-12-04 17:29:45 -0800

    starting command structure.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# electroncontrol
+# electron control
 minimal electron app that allows controlling the shell via stdin and stdout
+
+## Protocol
+The base of the protocol is base64 encoded JSON commands + bodies delimited by newline characters.
+
+The structure of commands is:
+```JSON
+{
+    "CommandID": "command_id",
+    "CommandBody": "command_body"
+}
+```
---
+++ app/stdiocommands.js
@@ -0,0 +1,33 @@
+readline = require('readline')
+
+// hold all of the commands that are registered.
+var commandMap {};
+var read = readline.createInterface({input: process.stdin});
+// Start - initializes and starts running the command queue.
+exports.Start = function () {
+    read.on('line', readlineCallback)
+}
+
+// Stop - stops listening on the stdin
+exports.Stop = function () {
+    read.Close()
+}
+
+// Listen - allows tne app to listen to a command that is sent to stdin.
+exports.Listen = function (commandID, fn) {
+    commandMap[commandID] = fn;
+}
+
+// readlineCallback - is called whenever the server sends a message to stdin.
+var readlineCallback = function (line) {
+    // line is base64 encoded so it needs to be decoded.
+    buffer = new Buffer(line, 'base64');
+    jsonData = buffer.toString();
+
+    // parse out the JSON into an object
+    obj = JSON.parse(jsonData);
+
+    // call the callback handler for that command.
+    callback = commandMap[obj.CommandID]
+    if (callback) callback(obj.CommandBody)
+}
