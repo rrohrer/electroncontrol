c6baba5f4eb355de5fc8297234d2c80b1642ce6a
Ryan Rohrer <ryan.rohrer@gmail.com>
2015-12-08 18:40:13 -0800

    removed all of the demo code from main.

--- app/main.js
+++ app/main.js
@@ -8,10 +8,6 @@ const ioCommands = require('./iocommands.js')
 // imports of local controllers.
 const WindowControl = require('./windowcontrol.js')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
@@ -30,23 +26,6 @@ app.on('ready', function() {
     // TODO: Send a handshake "ready to go" message.
     // start the command processor.
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('http://google.com')
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
   // TODO: move this to the beginning, once the handshake is in place.
   ioCommands.Start()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  });
 });
